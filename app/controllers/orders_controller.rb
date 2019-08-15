aa210f5ef157d903ad2925d3af3e3ca470c63734
Alexandra Vanhalme <alexandravanhalme@hotmail.com>
2019-08-15 17:37:10 +0200

    add show for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,17 @@ class OrdersController < ApplicationController
 
   def create
     # calculer le prix
-    Order.create(
-      starts_at: Date.new(params[:order]["starts_at(1i)"].to_i,params[:order]["starts_at(2i)"].to_i,params[:order]["starts_at(3i)"].to_i),
-      ends_at: Date.new(params[:order]["ends_at(1i)"].to_i,params[:order]["ends_at(2i)"].to_i,params[:order]["ends_at(3i)"].to_i),
+    @booking = Order.create(
+      starts_at: Date.new(params[:order]["starts_at(1i)"].to_i, params[:order]["starts_at(2i)"].to_i, params[:order]["starts_at(3i)"].to_i),
+      ends_at: Date.new(params[:order]["ends_at(1i)"].to_i, params[:order]["ends_at(2i)"].to_i, params[:order]["ends_at(3i)"].to_i),
       final_price: 0,
       user_id: current_user.id,
       pet_id: params[:pet_id]
     )
-    # redirect vers la show de l'order ou l'index des orders
   end
+
+  def show
+    @order = Order.find(params[:pet_id])
+  end
+  # redirect vers la show de l'order ou l'index des orders
 end
