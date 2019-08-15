e6296a770b7553834c94f4f4dea62eadd366a6d8
ejdataras <ejdaras@gmail.com>
2019-08-15 17:25:41 +0200

    controller done && order show added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pets#index'
   devise_for :users
   resources :pets, only: [:index, :show, :create, :new] do
-    resources :orders, only: [:new, :create]
+    resources :orders, only: [:new, :create, :show]
   end
 
 end
