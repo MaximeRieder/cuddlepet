b968d7cd381ee25cce37ffb075cf75dd00b23e74
Maxime Rieder <maximerieder@yahoo.com>
2019-08-16 14:04:06 +0200

    index scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-hh# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Pet.destroy_all
-User.destroy_all
+# Pet.destroy_all
+# User.destroy_all
 # users_list = [
 #   [ "max@yahoo.com", "81831000", 1000 ],
 #   [ "alex@gmail.com", "65447374", 1001 ],
