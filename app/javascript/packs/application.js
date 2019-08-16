5fbd0e0e2e379bc52e64a7c8028cf638f8b93522
ejdataras <ejdaras@gmail.com>
2019-08-16 10:52:11 +0200

    during

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
 
 loadDynamicBannerText();
 initMapbox();
